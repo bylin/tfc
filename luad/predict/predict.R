8d2abd2790bd5398ee235e48f0504807de6c5e81
Brian <me@brianl.in>
2016-02-12 15:21:45 -0800

    update sampling, auc plot functions

--- luad/predict/predict.R
+++ luad/predict/predict.R
@@ -7,15 +7,18 @@ setupTrainingTestingSets <- function(metadata, covariate, counts) {
   # metadata: filtered metadata for covariate of interest (e.g. no [Not Applicable] entries)
   # covariate: name of covariate (must be a column in metadata)
   # counts must be filtered for covariate of interest
+  num_samples_per_class <- min(table(metadata[, covariate]))/2
   random_sample <- unlist(sapply(levels(metadata[, covariate]), function(class) {
-    num_samples <- round(sum(metadata[, covariate] == class)/2)
-    sample(metadata[metadata[, covariate] == class, ]$participant_id, num_samples)
+    sample(metadata[metadata[, covariate] == class, ]$barcode, num_samples_per_class)
   }))
-  training_counts <-  counts[, metadata$participant_id %in% random_sample]
-  training_metadata <- metadata[metadata$participant_id %in% random_sample, ]
-  testing_counts <- counts[, !(metadata$participant_id %in% random_sample)]
-  testing_metadata <- metadata[!(metadata$participant_id %in% random_sample), ]
-  list(training_counts = training_counts, training_metadata = training_metadata, testing_counts = testing_counts, testing_metadata = testing_metadata, participant_ids = random_sample)
+  training_counts <-  counts[, metadata$barcode %in% random_sample]
+  training_metadata <- metadata[metadata$barcode %in% random_sample, ]
+  random_sample_2 <- unlist(sapply(levels(metadata[, covariate]), function(class) {
+    sample(metadata[metadata[, covariate] == class & !(metadata$barcode %in% random_sample), ]$barcode, num_samples_per_class)
+  }))
+  testing_counts <- counts[, metadata$barcode %in% random_sample_2]
+  testing_metadata <- metadata[metadata$barcode %in% random_sample_2, ]
+  list(training_counts = training_counts, training_metadata = training_metadata, testing_counts = testing_counts, testing_metadata = testing_metadata)
 }
 
 buildTestGlm <- function(dataset, features, covariate, randomize = FALSE) {
@@ -37,15 +40,46 @@ buildTestGlm <- function(dataset, features, covariate, randomize = FALSE) {
   list(roc = roc, coef = coef, auc = auc)
 }
 
+extractCoefs <- function(glms, ntrials) unlist(lapply(1:ntrials, function(trial) glms[[trial]]$coef))
+
 parseGlms <- function(glms, class) ldply(1:length(glms), function(i) data.frame(glms[[i]]$roc, Trial = i, AUC = glms[[i]]$auc, Class = class))
 
 plotGlms <- function(roc_df) ggplot(roc_df) + geom_line(aes(x = FPR, y = TPR, color = Class, group = paste0(Trial, Class)), alpha = 0.3) + geom_abline(intercept = 0, slope = 1, colour = "gray") + ylab("TPR") + xlab("FPR") + facet_wrap(~ Class, nrow = 2) + guides(color = FALSE)
 
-plotMeanAucs <- function(all_feature_glms) {
-  mean_aucs <- lapply(all_feature_glms, function(glms) {
-    aucs <- lapply(1:length(glms), function(i) glms[[i]]$auc)
-    mean(unlist(aucs))
-  })
-  df <- data.frame(AUC = unlist(mean_aucs), Class = as.factor(c("tsRNA", "miRNA", "snoRNA", "piRNA", "tRNA half", "tRNA")))
-  ggplot(df) + geom_bar(aes(x = Class, y = AUC), stat = "identity") + ggtitle("Mean AUC by sRNA type")
+plotAucs <- function(all_feature_glms) {
+  aucs <- ldply(all_feature_glms, function(glms) unlist(lapply(1:length(glms), function(i) glms[[i]]$auc)))
+  rownames(aucs) <- c("tsRNA", "miRNA", "snoRNA", "piRNA", "tRNA half", "tRNA")
+  df <- melt(t(aucs))[, -1]
+  colnames(df) <- c("sRNA", "AUC")
+  ggplot(df) + geom_boxplot(aes(x = sRNA, y = AUC)) + ggtitle("AUC distribution by sRNA type")
+}
+
+buildMultinomialGlm <- function(metadata, counts, covariate, features, randomize = FALSE) {
+  # set up training/testing sets. first, set parameters
+  dataset <- setupTrainingTestingSets(metadata, covariate, counts)
+  training_counts <- t(dataset$training_counts[rownames(dataset$training_counts) %in% features, ])
+  training_response <- dataset$training_metadata[match(colnames(dataset$training_counts), dataset$training_metadata$barcode), ][, covariate]
+  testing_counts <- t(dataset$testing_counts[rownames(dataset$testing_counts) %in% features, ])
+  testing_response <- dataset$testing_metadata[match(colnames(dataset$testing_counts), dataset$testing_metadata$barcode), ][, covariate]
+  if (randomize) training_response <- sample(training_response)
+
+  # build and test model
+  model <- cv.glmnet(training_counts, training_response, family = "multinomial", type.measure = "class", type.multinomial = "grouped")
+  pred <- predict(model, newx = testing_counts, s = "lambda.min", type = "class") 
+  unname(confusionMatrix(pred, testing_response)$overall[1])
+}
+
+buildCoxGlm <- function(metadata, counts, covariate, features, randomize = FALSE) {
+  # set up training/testing sets. first, set parameters
+  dataset <- setupTrainingTestingSets(metadata, covariate, counts)
+  training_counts <- t(dataset$training_counts[rownames(dataset$training_counts) %in% features, ])
+  training_response <- dataset$training_metadata[match(colnames(dataset$training_counts), dataset$training_metadata$barcode), ][, c("days_survived", covariate)]
+  testing_counts <- t(dataset$testing_counts[rownames(dataset$testing_counts) %in% features, ])
+  testing_response <- dataset$testing_metadata[match(colnames(dataset$testing_counts), dataset$testing_metadata$barcode), ][, covariate]
+  if (randomize) training_response <- sample(training_response)
+
+  # build and test model
+  model <- cv.glmnet(training_counts, training_response, family = "multinomial", type.measure = "class", type.multinomial = "grouped")
+  pred <- predict(model, newx = testing_counts, s = "lambda.min", type = "class") 
+  unname(confusionMatrix(pred, testing_response)$overall[1])
 }
